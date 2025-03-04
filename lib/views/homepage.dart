5e01c3d06dbef453eea53bd3893c407d9ec182f2
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-03-04 19:19:25 +0530

    screen

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_service.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -9,10 +13,73 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late WeatherData weatherInfo;
+  myWeather(){
+    WeatherService().fetchWeather().then((value){
+      setState(() {
+        weatherInfo = value;
+
+      });
+    });
+  }
+  @override
+  void initState() {
+    super.initState();
+    // TODO: implement initState
+    myWeather();
+
+  }
   @override
   Widget build(BuildContext context) {
+     var srcheight = MediaQuery.of(context).size.height;
     return Scaffold(
-      body:Stack(),
+      body:Stack(
+        children: [
+          Image.asset('assets/images/29.png',
+          filterQuality: FilterQuality.high,
+            // height: Get.height,
+            fit: BoxFit.cover,
+            height: Get.height*1,
+          ),
+          Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: Get.height*0.1),
+                  child: Text('Mullana',
+                    style: GoogleFonts.playfairDisplay(
+                        fontSize: Get.height*0.055,
+                      color: Colors.white
+                  ),),
+
+                ),
+                Text('19',
+                  style: GoogleFonts.cinzel(
+                    // fontWeight: FontWeight.normal,
+                      fontSize: Get.height*0.09,
+                    color: Colors.white
+                ),),
+                Text('Mostly Clear',
+                  style: GoogleFonts.dmSans(
+                    fontWeight: FontWeight.w700,
+                      fontSize: Get.height*0.02,
+                    color: Colors.grey
+                ),),
+      Text('H: 24 L:18',
+        style: GoogleFonts.allerta(
+            fontSize: Get.height*0.028,
+            color: Colors.white
+        ),),
+                Padding(
+                  padding: EdgeInsets.only(top: Get.height*0.13),
+                  child: Image.asset('assets/images/House 4 3.png',),
+                )
+              ],
+
+            ),
+          )
+        ],
+      ),
     );
   }
 }
