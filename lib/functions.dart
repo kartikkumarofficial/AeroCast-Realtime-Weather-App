206c2756d2e2f5603c54cef9951c848b8673dd84
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-03-26 22:16:02 +0530

    scrollsheet weather fetch

--- lib/functions.dart
+++ lib/functions.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 
-
+//general functions
+//function to capitalize first letter of each word
 String capitalizeEachWord(String text) {
   return text.split(' ').map((word) {
     if (word.isEmpty) return "";
